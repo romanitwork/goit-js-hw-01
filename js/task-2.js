205d069b13fe89ddfb59ee4cefc2d81b5e590171
romanitwork <workromanmail@gmail.com>
2020-01-19 17:08:40 +0200

    done task-2

---
+++ js/task-2.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const total = 200;
+const ordered = 199;
+
+if (total >= ordered) {console.log ('"Заказ оформлен, с вами свяжется менеджер"')}
+else { console.log ('"На складе недостаточно твоаров!"')};
