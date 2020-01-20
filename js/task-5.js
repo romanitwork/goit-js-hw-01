1931f32a4aa81c17db5d5ed85e600a5b68cf1c9f
romanitwork <workromanmail@gmail.com>
2020-01-20 14:01:20 +0200

    done task-5

--- js/task-5.js
+++ js/task-5.js
@@ -1,4 +1,46 @@
-'use strict';
+"use strict";
 
+let deliveryPrice;
 
+const countryDelivery = prompt("Куда везти товар?");
+const countryDeliveryLow = countryDelivery.toLowerCase();
 
+switch (countryDeliveryLow) {
+  case "китай":
+    deliveryPrice = 100;
+    alert(
+      `Доставка в ${countryDelivery} будет стоить ${deliveryPrice} кредитов`
+    );
+    break;
+
+  case "чили":
+    deliveryPrice = 250;
+    alert(
+      `Доставка в ${countryDelivery} будет стоить ${deliveryPrice} кредитов`
+    );
+    break;
+
+  case "австралия":
+    deliveryPrice = 170;
+    alert(
+      `Доставка в ${countryDelivery} будет стоить ${deliveryPrice} кредитов`
+    );
+    break;
+
+  case "индия":
+    deliveryPrice = 80;
+    alert(
+      `Доставка в ${countryDelivery} будет стоить ${deliveryPrice} кредитов`
+    );
+    break;
+
+  case "ямайка":
+    deliveryPrice = 120;
+    alert(
+      `Доставка в ${countryDelivery} будет стоить ${deliveryPrice} кредитов`
+    );
+    break;
+
+  default:
+    alert(`В вашей стране доставка не доступна`);
+}
