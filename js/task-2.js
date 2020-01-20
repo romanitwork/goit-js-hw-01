237f0ffad6db0d94225a73e7a9e16384cfd01854
romanitwork <workromanmail@gmail.com>
2020-01-20 15:11:51 +0200

    prettier fix

--- js/task-2.js
+++ js/task-2.js
@@ -1,7 +1,10 @@
-'use strict';
+"use strict";
 
 const total = 200;
 const ordered = 199;
 
-if (total >= ordered) {console.log ('"Заказ оформлен, с вами свяжется менеджер"')}
-else { console.log ('"На складе недостаточно товаров!"')};
+if (total >= ordered) {
+  console.log('"Заказ оформлен, с вами свяжется менеджер"');
+} else {
+  console.log('"На складе недостаточно товаров!"');
+}
--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,14 @@
-'use strict';
+"use strict";
 
-const password = prompt('Пожалуйста, введите пароль:');
-const ADMIN_PASSWORD = 'jqueryismyjam';
+const password = prompt("Пожалуйста, введите пароль:");
+const ADMIN_PASSWORD = "jqueryismyjam";
 
-if (password === null){console.log('Отменено пользователем!')}
-else{
-if (password === ADMIN_PASSWORD){console.log('Добро пожаловать!')}
-else{
-    console.log('Доступ запрещен, неверный пароль!')}
+if (password === null) {
+  console.log("Отменено пользователем!");
+} else {
+  if (password === ADMIN_PASSWORD) {
+    console.log("Добро пожаловать!");
+  } else {
+    console.log("Доступ запрещен, неверный пароль!");
+  }
 }
-
